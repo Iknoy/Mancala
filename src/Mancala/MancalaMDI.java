0c1b7b93a987c60bd790c4c1444944607fe25bcb
Jonathan Nicio <jnicion@gmail.com>
2019-08-10 21:41:02 -0500

    feature: Create main class

---
+++ src/Mancala/MancalaMDI.java
@@ -0,0 +1,16 @@
+
+package Mancala;
+
+/**
+ *
+ * @authors Jonathan Nicio, Victor Duran, Edgar Medel
+ */
+public class MancalaMDI {
+
+
+    public static void main(String[] args) {
+        Tablero NewGame = new Tablero();
+        NewGame.crearJuego();
+    }
+
+}
